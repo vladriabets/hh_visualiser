ae18d6f4e2bedecd6baeb7b086fe01c9927f478e
Vladislav <40403012+vladriabets@users.noreply.github.com>
2019-10-16 15:03:35 +0300

    Update visualiser.py

--- visualiser.py
+++ visualiser.py
@@ -1,8 +1,9 @@
 from matplotlib import pyplot as plt
 import requester
 
+text = 'python'
 
-skills = requester.get_most_demanded_skills(30, requester.count_key_skills(requester.get_vacancies('python')))
+skills = requester.get_most_demanded_skills(30, requester.count_key_skills(requester.get_vacancies(text)))
 names = [name for number, name in skills]
 values = [number for number, name in skills]
 
