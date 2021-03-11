8c28de27272b60aad52110b18aec994492611c83
vladriabets <vladriabets@yandex.ru>
2021-03-11 23:03:13 +0300

    refactored visualizer. now it works with async requester version

--- visualizer.py
+++ visualizer.py
@@ -4,6 +4,22 @@ from matplotlib import pyplot as plt
 import requester
 
 
+def create_plot(number, text, bars, numbers):
+    bars = [bar.replace(' ', '\n') for bar in bars]
+
+    fig, ax = plt.subplots()
+    fig.set_figheight(6)
+    fig.set_figwidth(10)
+    ax.set(
+        title = '{} наиболее востребованных навыков для {}'.format(number, text),
+    )
+
+    ax.barh(bars, numbers, color = 'darkblue')
+
+    plt.show()
+    fig.savefig('plot.png')
+
+
 def main():
     user_input = requester.get_args()
 
@@ -11,13 +27,11 @@ def main():
     main_number = user_input[1]
 
     skills_count = asyncio.run(requester.skills_count(main_text, main_number))
+    skills_count.reverse()
     names = [name for name, number in skills_count]
     values = [number for name, number in skills_count]
 
-    fig = plt.figure()
-    plt.bar(names, values)
-    plt.show()
-    fig.savefig('plot.png')
+    create_plot(main_number, main_text, names, values)
 
 
 if __name__ == "__main__":
